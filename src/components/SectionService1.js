fb02648e3f77240433263bb5cac968a30f2ad9cb
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-01-21 19:27:40 +0100

    update on services

--- src/components/SectionService1.js
+++ src/components/SectionService1.js
@@ -5,36 +5,37 @@ const Section1 = () => {
     {
       id: 1,
       icon: "far fa-gem diamond",
-      heading: "Hit the ground running.",
+      heading:
+        "Engineering, Procurement, & Construction Management (EPCM) Contracts",
       content:
-        "We frontload the work from first contact, packing months of strategy into your initial appraisal and proposal.",
+        "For familiar or reoccurring projects with which you prefer to keep your hand on the rudder, put Zicad Integrated Engineering services to work.  We can work alongside your resources to help steer your project towards success while you rely on the proven procedures and processes you are accustomed to.",
     },
     {
       id: 2,
       icon: "fas fa-chart-bar",
-      heading: "Only the best in your corner.",
+      heading: "Engineering, Procurement, & Construction (EPC) Contracts",
       content:
-        "We frontload the work from first contact, packing months of strategy into your initial appraisal and proposal.",
+        "For projects that may lie outside of your comfort sphere, let Zicad Integrated Engineering services assume single-point accountability from start to finish.  We can offer a turnkey product/service guarantee with the security of a comprehensive project estimate.  Let us reduce your administrative burden so you can focus your resources elsewhere.  Our strategic alliances can help shorten timelines, reduce costs, and keep processes and documentation lean by tailoring each need to specific project demands.",
     },
   ];
   return (
     <div className="container Section1">
       <div className="section-headings">
         <h1 className="Section1-heading">
-          The thing that motivates me is a very common form of motivation. And
-          that is, with other folk counting on me, it's so easy to be motivated.
+          ....At Zicad Integrated Engineering services
         </h1>
         <p className="Section1-content">
-          we work with business & provide solution to client with their business
-          problem
+          We undertake both EPCM and EPC contracts which are further described
+          below:
         </p>
       </div>
       <div className="Section1-Services">
         <div className="serviced-Items">
-          <div className="row">
+          <div className="row11">
             {Services1.map((item) => {
               return (
-                <div key={item.id} className="col-md-3 secrviceSection-column">
+                // <div key={item.id} className="col-md-3 secrviceSection-column">
+                <div key={item.id} className="contracts__section">
                   <i className={item.icon}></i>
                   <h3 className="serviceSection-heading">{item.heading}</h3>
                   <p className="serviceSection-content">{item.content}</p>

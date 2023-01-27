18c4025c3be27ce4d297f288d4bd9eeb694c1847
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-01-27 07:29:53 +0100

    services functionality

---
+++ src/components/mepservices/ServicesContent.js
@@ -0,0 +1,24 @@
+import mechanicalIcon from './ServiceImg/icons8-gear-100.png'
+import electricalIcon from './ServiceImg/icons8-electrical-80.png'
+import pipingIcon from './ServiceImg/icons8-pipe-60.png'
+
+export const serviceContents = [
+  {
+    serviceTitle: 'Mechanical',
+    serviceText: ``,
+    serviceIcon: `${mechanicalIcon}`,
+    serviceDelay: 0,
+  },
+  {
+    serviceTitle: 'Electrical',
+    serviceText: ``,
+    serviceIcon: `${electricalIcon}`,
+    serviceDelay: 200,
+  },
+  {
+    serviceTitle: 'Piping',
+    serviceText: ``,
+    serviceIcon: `${pipingIcon}`,
+    serviceDelay: 400,
+  },
+]

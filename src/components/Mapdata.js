27a7d992df4a6c71951f7338a412c1105c63629e
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-02-23 06:27:26 +0100

    contact page

---
+++ src/components/Mapdata.js
@@ -0,0 +1,51 @@
+import "./mapstyle.css";
+import Map from "./Map";
+// import Map from "./Map/Map";
+import { useEffect, useState } from "react";
+export default function Mapp() {
+  const [coords, setCorrds] = useState({
+    latitude: 6.59560,
+    longitude: 3.3581
+    // latitude: 6.5953987,
+    // longitude: 3.3560892
+  });
+  const [display_name, setName] = useState("Our Office");
+
+  const [address, setAddress] = useState({});
+
+  function error() {
+    alert('Sorry, no position available.');
+  }
+  const options = {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 27000
+  };
+
+  function getCurrentCityName( ){
+    
+   let url="https://nominatim.openstreetmap.org/reverse?format=jsonv2"+
+   "&lat="+coords.latitude+"&lon="+coords.longitude;
+
+    fetch(url, {
+      method: "GET",   
+      mode: 'cors', 
+      headers: {
+        "Access-Control-Allow-Origin": "https://o2cj2q.csb.app"
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => setName( data.display_name));
+  }
+
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <div className="App">
+      {console.log(coords)}
+      <Map coords={coords} display_name={display_name} />
+    </div>
+  );
+}
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer1 from '../components/footer1'
 import Map from '../components/Map'
+import Mapp from '../components/Mapdata'
 
 //
 // ========
@@ -13,17 +14,25 @@ const Contacts = function() {
         style={{
           marginTop: '5rem',
           display: 'flex',
-          justifyContent: 'center',
+          // justifyContent: 'space-between',
         }}
       >
         <div>
-          Address: 11, agbaoku street, off awosika bus stop, opebi Lagos,
-          Nigeria.
-        </div>
-        <div>Telephone: +2348-162-728-300 | +2348-053-454-579</div>
+          zicad Integrated Engineering services
+          <div>Address: 11, agbaoku street, off awosika bus stop, opebi Lagos,
+          Nigeria.</div>
+          <div>Telephone: +2348-162-728-300 | +2348-053-454-579</div>
         <div>zicadintegrated@gmail.com</div>
+        </div>
+        <Mapp />
       </section>
-      <Map />
+      <form>
+        <input type={Text} placeholder='Your Name'></input>
+        <input type={Number} placeholder='Surname'></input>
+        <input type={Number} placeholder='Your Email'></input>
+        <button type='submit' onClick={()=> sendmail}>Send Message</button>
+      </form>
+      
       <section style={{ position: 'absolute', bottom: '0', width: '100%' }}>
         <Footer1 />
       </section>

c38e7a549662fcaf90f11711c3ccf75e267ee71d
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-02-23 14:35:17 +0100

    emailjs implementation

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -2,11 +2,24 @@ import React from 'react'
 import Footer1 from '../components/footer1'
 import Map from '../components/Map'
 import Mapp from '../components/Mapdata'
+import emailjs from 'emailjs-com'
 
 //
 // ========
 
 const Contacts = function() {
+  
+  function sendEmail(e) {
+    e.preventDefault();    //To prevent reload
+
+    emailjs.sendForm('service_duv7ooh', 'template_6952ogo', e.target, 'GsOug08hfQqbgkERA')
+      .then((result) => {
+          window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
   return (
     <>
       <h2>Company Contact Information</h2>
@@ -26,12 +39,19 @@ const Contacts = function() {
         </div>
         <Mapp />
       </section>
-      <form>
-        <input type={Text} placeholder='Your Name'></input>
-        <input type={Number} placeholder='Surname'></input>
-        <input type={Number} placeholder='Your Email'></input>
-        <button type='submit' onClick={()=> sendmail}>Send Message</button>
-      </form>
+
+      <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" required/>
+      <label>Name</label>
+      <input type="text" name="from_name" required/>
+      <label>Email</label>
+      <input type="email" name="from_email" required/>
+      <label>Subject</label>
+      <input type="text" name="subject" required/>
+      <label>Message</label>
+      <textarea name="html_message" />
+      <input type="submit" value="Send" />
+    </form>
       
       <section style={{ position: 'absolute', bottom: '0', width: '100%' }}>
         <Footer1 />
